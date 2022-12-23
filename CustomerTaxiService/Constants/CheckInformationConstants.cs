695bac682624bbc884372448ef0fa22aefd2aff7
Nik Nedelko <nedelko1nik@gmail.com>
2022-12-23 19:07:50 +0100

    Add new constants

--- CustomerTaxiService/Constants/CheckInformationConstants.cs
+++ CustomerTaxiService/Constants/CheckInformationConstants.cs
@@ -5,4 +5,6 @@ public static class CheckInformationConstants
     public const string Ok = "Ok";
     public const string UserNotFound = "User not found";
     public const string UserIsExist = "User is exist";
+    public const string RideNotFound = "Ride not found";
+    public const string DatabaseProblems = "Problem with database";
 }
