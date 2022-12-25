443302689a68a3aa2b7992af3e5231a090218f70
Nik Nedelko <nedelko1nik@gmail.com>
2022-12-25 15:31:02 +0100

    update interface

--- CustomerTaxiService/Repository/Interfaces/IUserRepository.cs
+++ CustomerTaxiService/Repository/Interfaces/IUserRepository.cs
@@ -5,9 +5,9 @@ namespace CustomerTaxiService.Repository.Interfaces;
 public interface IUserRepository
 {
     public Task<string> AddNewUser(Customer customer);
-    public Task<string> RemoveUser(string name);
+    public Task<string> RemoveUser(Customer userEntity);
     public Task<Customer?> GetUserByPhoneNumber(string number);
-    public Task<string?> PermissionToRide(string name);
-    public Task<string> CheckOfExist(string name);
-    public Task<string> UpdateUser(string name);
+    public Task<string?> PermissionToRide(string userId);
+    public Task<string> CheckOfExist(string phoneNumber);
+    public Task<Customer?> UpdateUser(Customer user, string existUserId);
 }
