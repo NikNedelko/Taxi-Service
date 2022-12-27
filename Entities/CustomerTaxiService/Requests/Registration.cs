889284eb2bfc7409cc21b18d3af3bb540e8f97d3
Nik Nedelko <nedelko1nik@gmail.com>
2022-12-27 18:58:45 +0100

    add simple validation for registration model

--- Entities/CustomerTaxiService/Requests/Registration.cs
+++ Entities/CustomerTaxiService/Requests/Registration.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Entities.CustomerTaxiService.Requests;
 
 [Serializable]
 public class Registration
 {
+    [Required]
+    [StringLength(15)]
     public string Name { get; set; }
+    [Required]
+    [StringLength(15)]
     public string Surname { get; set; }
+    [Required]
+    [Range(100000,999999)]
     public string PhoneNumber { get; set; }
+    [Required]
+    [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")]
     public string Email { get; set; }
 }
