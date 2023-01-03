92cba1b7e9118a24a6959cab23e77dff05fe8f1c
Nik Nedelko <nedelko1nik@gmail.com>
2023-01-03 20:08:01 +0100

    feat: add driver entity #3

---
+++ Entities/DriverApi/Driver/Driver.cs
@@ -0,0 +1,16 @@
+using Entities.General;
+
+namespace Entities.DriverApi.Driver;
+
+public class Driver
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string DriverLicenseNumber { get; set; }
+    public string Car { get; set; }
+    public DriveClass DriveClass { get; set; }
+    public AccountStatus Status { get; set; }
+    public FeedBack FeedBack { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    public decimal Balance { get; set; }
+}
---
+++ Entities/DriverApi/Driver/DriverDB.cs
@@ -0,0 +1,15 @@
+namespace Entities.DriverApi.Driver;
+
+public class DriverDB
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string DriverLicenseNumber { get; set; }
+    public string Car { get; set; }
+    public int DriveClass { get; set; }
+    public int Status { get; set; }
+    public int FeedBack { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    public decimal Balance { get; set; }
+}
