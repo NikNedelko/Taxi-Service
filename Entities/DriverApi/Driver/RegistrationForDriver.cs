a857ef5cf89e165e7c18300cb92332d1284ceed8
Nik Nedelko <nedelko1nik@gmail.com>
2023-01-04 19:24:07 +0100

    feat: add entity for registration #3

---
+++ Entities/DriverApi/Driver/RegistrationForDriver.cs
@@ -0,0 +1,9 @@
+namespace Entities.DriverApi.Driver;
+
+public class RegistrationForDriver
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string DriverLicenseNumber { get; set; }
+    public string Car { get; set; }
+}
