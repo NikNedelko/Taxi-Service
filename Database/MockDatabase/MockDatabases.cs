2bd4d4f69aa9efc319b50e71d2378900625a2507
Nik Nedelko <nedelko1nik@gmail.com>
2023-01-08 18:38:07 +0100

    feat: create mock database version for Rides #5

---
+++ Database/MockDatabase/MockDatabases.cs
@@ -0,0 +1,23 @@
+﻿using Entities.General;
+using Entities.General.RideData;
+
+namespace Database.MockDatabase;
+
+public static class MockRideDatabase
+{
+    public static List<RideDb> RideList = new()
+    {
+        new RideDb
+        {
+            Id = 1,
+            DriverPhoneNumber = "1234",
+            CustomerPhoneNumber = "1234",
+            IsTaken = false,
+            IsEnd = false,
+            EndPointOfRide = "Heaven",
+            RideDate = DateTime.Today,
+            DriverFeedBack = (int)FeedBack.Normal,
+            CustomerFeedBack = (int)FeedBack.Normal
+        }
+    };
+}
