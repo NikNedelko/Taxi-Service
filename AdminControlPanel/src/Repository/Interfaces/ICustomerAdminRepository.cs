a300253d0cc2d51c7182cea37b77268c5cd69e0c
Nik Nedelko <nedelko1nik@gmail.com>
2023-01-13 19:27:14 +0100

    feat: add logic to repository and interface #6

--- AdminControlPanel/src/Repository/Interfaces/ICustomerAdminRepository.cs
+++ AdminControlPanel/src/Repository/Interfaces/ICustomerAdminRepository.cs
@@ -1,9 +1,13 @@
 using CustomerTaxiService.Repository.Interfaces;
 using Entities.CustomerTaxiService.CustomerData;
+using Entities.General;
 
 namespace AdminControlPanel.Repository.Interfaces;
 
 public interface ICustomerAdminRepository : IUserRepository
 {
     public Task<List<CustomerDB>> GetAllUserWIthId();
+    public Task<string> DeleteUserById(int userId);
+    public Task<CustomerDB?> GetUserById(int userId);
+    public Task<string> ChangeCustomerStatus(string phoneNumber, AccountStatus newAccountStatus);
 }
--- AdminControlPanel/src/Repository/MockCustomerAdminRepository.cs
+++ AdminControlPanel/src/Repository/MockCustomerAdminRepository.cs
@@ -1,12 +1,51 @@
 using AdminControlPanel.Repository.Interfaces;
+using CustomerTaxiService.Repository.Interfaces;
+using Database.MockDatabase;
 using Entities.CustomerTaxiService.CustomerData;
+using Entities.General;
 
 namespace AdminControlPanel.Repository;
 
 public abstract class MockCustomerAdminRepository : ICustomerAdminRepository
 {
+    private IUserRepository _customerRepository;
+
+    protected MockCustomerAdminRepository(IUserRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+
+    public async Task<List<CustomerDB>> GetAllUserWIthId()
+    {
+        return MockDatabases.CustomerList;
+    }
+
+    public async Task<string> DeleteUserById(int userId)
+    {
+        MockDatabases.CustomerList.Remove(MockDatabases.CustomerList.FirstOrDefault(x => x.Id == userId));
+        return "Ok";
+    }
+
+    public async Task<CustomerDB?> GetUserById(int userId)
+    {
+        return MockDatabases.CustomerList.FirstOrDefault(x => x.Id == userId);
+    }
+
+    public async Task<string> ChangeCustomerStatus(string phoneNumber, AccountStatus newAccountStatus)
+    {
+        var entity = await GetCustomerDbByPhoneNumber(phoneNumber);
+        entity.Status = (int)newAccountStatus;
+        MockDatabases.CustomerList.Remove(MockDatabases.CustomerList.FirstOrDefault(x => x.PhoneNumber == phoneNumber));
+        MockDatabases.CustomerList.Add(entity);
+        return "Ok";
+    }
     
-    
+    private async Task<CustomerDB?> GetCustomerDbByPhoneNumber(string phoneNumber)
+    {
+        return MockDatabases.CustomerList.FirstOrDefault(x => x.PhoneNumber == phoneNumber);
+    }
+
+
     public abstract Task<string> AddNewUser(Customer customer);
     public abstract Task<string> RemoveUser(string phoneNumber);
     public abstract Task<Customer?> GetUserByPhoneNumber(string number);
