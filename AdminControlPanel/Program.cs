47ae093f42601d007a5e2b99d76d0b1897d75679
Nik Nedelko <nedelko1nik@gmail.com>
2023-01-17 16:04:58 +0100

    feat: add dependencies #6

--- AdminControlPanel/Program.cs
+++ AdminControlPanel/Program.cs
@@ -1,9 +1,23 @@
 using AdminControlPanel.BL;
 using AdminControlPanel.BL.Interfaces;
+using AdminControlPanel.Repository;
+using AdminControlPanel.Repository.Interfaces;
+using CustomerTaxiService.Repository.Interfaces;
+using CustomerTaxiService.Repository.MockRepository;
+using DriverTaxiService.Repository.Interfaces;
+using DriverTaxiService.Repository.MockRepository;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IAccountLogicForAdmin,CustomerControlLogic >();
+builder.Services.AddScoped<IUserRepository, MockUsersRepository>();
+builder.Services.AddScoped<IAccountLogicForAdmin, CustomerControlLogic>();
+
+builder.Services.AddScoped<IDriverAccountRepository, MockDriverAccountRepository>();
+builder.Services.AddScoped<IDriversLogicForAdmin, DriversLogicForAdmin>();    
+
+builder.Services.AddScoped<ICustomerAdminRepository, MockCustomerAdminRepository>();
+builder.Services.AddScoped<IDriverAdminRepository, MockDriverAdminRepository>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
