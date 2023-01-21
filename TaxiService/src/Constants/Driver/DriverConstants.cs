c3f5e81ecacb4d8845cf4b688ace641497c8155a
Nik Nedelko <nedelko1nik@gmail.com>
2023-01-21 19:58:11 +0100

    fix: create new constant file and remove old

--- TaxiService/src/Constants/Driver/DriverConstants.cs
+++ TaxiService/src/Constants/Driver/DriverConstants.cs
@@ -1,15 +1,23 @@
-namespace TaxiService.Constants.Driver.AccountConstants;
+namespace TaxiService.Constants.Driver;
 
-public static class AccountConstants
+public static class DriverConstants
 {
     public const string Ok = "Ok";
+    public const string DriverIsNotWorking = "Driver is not working";
+    public const string OrderByIdIsNotExist = "Order with this id is not exist";
+    public const string OrderIsAlreadyTaken = "Order with this id is already taken";
+    public const string OrderByNumberIsNotExist = "Order with this phone number is not exist";
+    public const string CanNotDeleteWhileInRide = "You can not delete account while you in a ride";
+    
+    public const string OrderByIdIsNotExistAdditionalText = "Please check id of order";
+    public const string OrderByNumberIsNotExistAdditionalText = "Please check your phone number of order";
+    
     
     public const string DriverIsNotExist = "Driver is not exist";
     public const string DriverIsExist = "Driver is already exist";
     public const string DriverWasAdded = "Your driver account was created";
     public const string DriverWasDeleted = "Your driver account was deleted";
     
-    public const string DriverIsNotWorking = "Driver is not working";
     public const string DriverIsAlreadyWorking = "Driver account is already working";
     
     public const string DriverWasDeletedAdditionalInfo = "Sorry to hear that. We hope to see you again";
@@ -17,6 +25,4 @@ public static class AccountConstants
 
     public const string DriverIsExistAdditionalInfo = "Driver with this phone number or license number is already exist";
     public const string DriverWasAddedAdditionalInfo = "Thank you for registration in our service. Now you can start work and take your first order";
-
-
 }
