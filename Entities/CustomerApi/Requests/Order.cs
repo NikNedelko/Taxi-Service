148fdfc3fa4f2c061d9d44eada679eb556306c1b
Nik Nedelko <nedelko1nik@gmail.com>
2023-01-24 18:47:43 +0100

    feat: add new field and attribute #9

--- Entities/CustomerApi/Requests/Order.cs
+++ Entities/CustomerApi/Requests/Order.cs
@@ -1,8 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using Entities.DriverApi;
+
 namespace Entities.CustomerApi.Requests;
 
 [Serializable]
 public class Order
 {
+    [Required]
     public string PhoneNumber { get; set; }
+    [Required]
     public string RideEndPoint { get; set; }
+    [Required]
+    [Range(0,50000)]
+    public decimal Price { get; set; }
+    [Required]
+    public DriveClass DriveClass { get; set; }
 }
