8203b4ee36ea59f3dcc3d6ab448566bda720e6a0
Nik Nedelko <nedelko1nik@gmail.com>
2023-01-28 21:59:04 +0100

    feat: create constants file #6

---
+++ AdminControlPanel/src/Constants/AdminConstants.cs
@@ -0,0 +1,10 @@
+namespace AdminControlPanel.Constants;
+
+public static class AdminConstants
+{
+    public const string Ok = "Ok";
+    public const string UserWithThisNumberIsNotExist = "User with this phone number is not exist";
+    public const string UserIsNotExist = "User is not exist";
+    public const string UserIsExist = "User is exist";
+    public const string UserNotFound = "User is exist";
+}
