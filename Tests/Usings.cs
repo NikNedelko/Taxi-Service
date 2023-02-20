c374991a8cc4e11c1379082525877cb552e33942
Nik Nedelko <nedelko1nik@gmail.com>
2023-02-20 19:34:26 +0100

    feat: add new using to global using

--- Tests/Usings.cs
+++ Tests/Usings.cs
@@ -1,1 +1,6 @@
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using Database.MockDatabase;
+global using Entities.CustomerApi.Requests;
+global using TaxiService.BusinessLogic.Customer.Interfaces;
+global using TaxiService.Constants.Customer;
+global using NSubstitute;
