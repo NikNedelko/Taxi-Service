dc22e296782fb33e995b4b3fd8700428c821153a
Nik Nedelko <nedelko1nik@gmail.com>
2023-02-23 16:55:09 +0100

    feat: add constant class #11

---
+++ Tests/Unit/CustomerTests/CustomerTestsConstants.cs
@@ -0,0 +1,19 @@
+namespace Tests.Unit.CustomerTests;
+
+public static class CustomerTestsConstants
+{
+    public const string Registration_Name = "Registration_TestNameForUnitTesting";
+    public const string Registration_LastName = "Registration_TestLastNameForUnitTesting";
+    public const string Registration_Phonenumber = "Registration_TestPhoneNumberForUnitTesting";
+    public const string Registration_Email = "Registration_TestEmailForUnitTesting";
+
+    public const string UserDb_Name = "TestNameForUnitTesting";
+    public const string UserDb_LastName = "TestLastNameForUnitTesting";
+    public const string UserDb_Phonenumber = "TestPhoneNumberForUnitTesting";
+    public const string UserDb_Email = "TestEmailForUnitTesting";
+    
+    public const string Update_Name = "Update_TestNameForUnitTesting";
+    public const string Update_LastName = "Update_TestLastNameForUnitTesting";
+    public const string Update_Phonenumber = "TestPhoneNumberForUnitTesting";
+    public const string Update_Email = "Update_TestEmailForUnitTesting";
+}
