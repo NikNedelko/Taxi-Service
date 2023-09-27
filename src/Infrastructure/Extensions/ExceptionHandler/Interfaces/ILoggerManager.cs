f7332ef7e297969dcfc11f4d7cf016d37737b350
Nik Nedelko <nedelko1nik@gmail.com>
2023-09-27 09:41:00 +0200

    fix: remove old exception handler and useless cs files #12

--- src/Infrastructure/Extensions/ExceptionHandler/Interfaces/ILoggerManager.cs
+++
@@ -1,9 +0,0 @@
-namespace Infrastructure.Extensions.ExceptionHandler.Interfaces;
-
-public interface ILoggerManager
-{
-    void LogInfo(string message);
-    void LogWarn(string message);
-    void LogDebug(string message);
-    void LogError(string message);
-}
